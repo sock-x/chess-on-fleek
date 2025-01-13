b70e0f7419c9fa6f6f9717e1a74a40f5801aa60f
sock-x <carlos.delgado706@gmail.com>
2025-01-13 18:21:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+   // pages/index.js
+   export default function Home() {
+     return (
+       <div>
+         <h1>Welcome to Chess on Fleek!</h1>
+         <p>This is a placeholder for your application content.</p>
+       </div>
+     );
+   }
