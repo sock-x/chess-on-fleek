6863339ddf6925833db661c7efe39007347e9a2e
sock-x <carlos.delgado706@gmail.com>
2025-01-15 18:51:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
    // pages/index.js
-   export default function Home() {
-     return (
-       <div>
-         <h1>Welcome to Chess on Fleek!</h1>
-         <p>This is a placeholder for your application content.</p>
-       </div>
-     );
-   }
+
+     import React from 'react';
+
+     const Home = () => {
+       return (
+         <div>
+           <h1>Welcome to Chess on Fleek!</h1>
+           <p>This is a placeholder for your application content.</p>
+         </div>
+       );
+     };
+
+     export default Home;
